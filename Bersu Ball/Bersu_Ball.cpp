545fffcc6b88f90762e85a16c4856f905456b861
SanskritiPatelCSE <163129303+SanskritiPatelCSE@users.noreply.github.com>
2024-03-19 17:31:35 +0530

    Add files via upload

---
+++ Bersu Ball/Bersu_Ball.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+#define ll long long
+#define srv(v) sort(v.begin(), v.end())
+#define fo(i, a, b) for (ll i = a; i < b; i++)
+#define str string
+#define sz size()
+#define dv(v) vector<ll> v
+#define ds(s) set<ll> s
+#define dm(mp) map<ll, ll> mp
+using namespace std;
+const int M = 1e9 + 7;
+const int N = 1e5 + 7;
+
+int k;
+int h[N];
+int dp[N];
+
+int func(int i)
+{
+    if (i == 0)
+        return 0;
+    if (dp[i] != -1)
+        return dp[i];
+    int cost = INT_MAX;
+    for (int j = 1; j <= k; j++)
+    {
+        if (i - j >= 0)
+            cost = min(cost, func(i - j) + abs(h[i] - h[i - j]));
+    }
+    return dp[i] = cost;
+}
+
+int main(){
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    ll m;
+    cin >> m;
+    ll b[m];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+
+    sort(a, a + n);
+    sort(b, b + m);
+    ll count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (b[j] != -1)
+            {
+                if (abs(a[i] - b[j]) <= 1)
+                {
+                    ++count;
+                    b[j] = -1;
+                    break;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+}
